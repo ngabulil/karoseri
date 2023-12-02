f0c821838e4342d245d5d05d09808645e260e789
Yoga Ulil Al Baab <yogaulilulul@bluebirdgroup.com>
2023-12-02 22:00:30 +0700

    fix padding navbar

--- src/Components/navbar/Navbar.tsx
+++ src/Components/navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import RoutesNav from "./RoutesNav";
 const Navbar = () => {
   return (
     <div className="bg-slate-200">
-      <div className="flex px-12 py-6 justify-between items-center">
+      <div className="flex px-12 py-6 justify-between items-center sm:px-4">
         <Logo />
         <RoutesMob />
         <RoutesNav />
